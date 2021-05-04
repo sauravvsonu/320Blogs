0d3a5ba601de71c5335acef18da488e361c5ad22
sauravvsonu <54908611+sauravvsonu@users.noreply.github.com>
2021-05-04 16:59:24 +0530

    logo bug

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ProjectSummary = ({ project }) => {
     return (
         <div>
-            <div className="card z-depth-0 project-summary" >
+            <div className="card z-depth-0 project-summary">
                 <div className="card-content black-text text-darken-3">
                     <span className="card-title">{project.title}</span>
                     <p>Posted by {project.authorFirstName} {project.authorLastName} </p>

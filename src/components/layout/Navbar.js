909d167e3330eeda12f9ede4c10e187c1d53595c
sauravvsonu <54908611+sauravvsonu@users.noreply.github.com>
2021-05-04 17:14:43 +0530

    bug

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -12,11 +12,7 @@ const Navbar = (props) => {
   return (
     <nav className="nav-wrapper" style={{ background: "rgba(0,0,0,0.95)" }}>
       <div className="container">
-<<<<<<< HEAD
         <Link to='/' style={{ fontSize: '18px', fontWeight: 600, letterSpacing: '2px', float: 'left', position: 'fixed' }}>320Blog</Link>
-=======
-        <Link to='/' style={{ fontWeight: 600, letterSpacing: '2px', float: 'left', fontSize: '20px' }}>320Blog</Link>
->>>>>>> 76be2fa75198102bd139afc2333c413a21305441
         {auth.isLoaded && links}
       </div>
     </nav>
